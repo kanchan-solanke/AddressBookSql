187d5f629a24c222ec04dcc0878d26f29b2e96d1
kanchan-solanke <kanchanssolanke@gmail.com>
2021-08-23 07:54:00 +0530

    Create table

--- UC1.sql
+++
@@ -1,1 +0,0 @@
-create database AddressBook_Service
---
+++ UC2.sql
@@ -0,0 +1,8 @@
+>create database AddressBook_Service;
+
+>select database();
+
+>use AddressBook_Service;
+
+>create table AddressBook_Service(FirstName varchar(45), LastName varchar(45), Address varchar(50), City varchar(45),
+state varchar(45), EmailId varchar(45), Mobile  varchar(45), pincode varchar(30));
