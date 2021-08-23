b0ad6618e7371c86f822c509e65bb24d2cb5c0a1
kanchan-solanke <kanchanssolanke@gmail.com>
2021-08-23 08:06:21 +0530

    Insert new Contact

--- UC3.sql
+++ UC3.sql
@@ -6,3 +6,8 @@
 
 >create table AddressBook_Service(FirstName varchar(45), LastName varchar(45), Address varchar(50), City varchar(45),
 state varchar(45), EmailId varchar(45), Mobile  varchar(45), pincode varchar(30));
+
+>select database();
+
+>Insert into AddressBook_Service(FirstName, LastName, Address, City, State, EmailId, Mobile,pincode)
+values('Kanchan', 'Solanke', 'Lake Street', 'Pune','Maharashtra','abc@gmail.com','9123456789','123456');
